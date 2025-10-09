8fa2f9f512b09f4f23c169e8313f26ab4848b739
Wilson Ibekason <102219342+wilsonibekason@users.noreply.github.com>
2025-10-09 16:08:37 +0100

    refactor: update global type declarations for tabManager to allow undefined

--- scripts/global.d.ts
+++ scripts/global.d.ts
@@ -1,6 +1,7 @@
 // Global type declarations for window extensions
 
 interface Window {
-  tabManager: import("./tabs").TabManager;
+  //   tabManager: import("./tabs").TabManager;
+  tabManager: import("./tabs").TabManager | undefined;
   createNewTab: () => void;
 }
